5a2d7718fc61bb8a2118deecadbc641fea51a9f5
Fabi2094 <68891397+Fabi2094@users.noreply.github.com>
2020-10-16 21:03:51 -0600

    Update PersonList

--- src/PersonList.cpp
+++ src/PersonList.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "PersonList.h"
+
+PersonList::PersonList() {
+
+}
+
+
+PersonList::~PersonList() {
+    personList.clear();
+}
+
+void PersonList::addPerson(Person person) {
+    personList.push_back(person);
+}
+
+Person PersonList::getPerson(int position) {
+    return personList.at(position);
+}
+
+int PersonList::getListSize() {
+    return personList.size();
+}
+
+std::string PersonList::toString() {
+    std::ostringstream output;
+    for (int i; i < personList.size(); i++) {
+        output << personList[i].toString();
+    }
+    return output.str();
+}
+
--- src/PersonList.h
+++ src/PersonList.h
@@ -6,7 +6,19 @@
 #define LAB07_FILES_SERIALIZATION_FABI2094_PERSONLIST_H
 
 
+#include"Person.h"
+#include<vector>
+
 class PersonList {
+private:
+    std::vector<Person> personList;
+public:
+    PersonList();
+    ~PersonList();
+    void addPerson(Person person);
+    Person getPerson(int position);
+    int getListSize();
+    std::string toString();
 
 };
 
